5304bbeb8c94a6d8c55629cefb1be46f3206f687
karam kim <karamkim88@lguplus.co.kr>
2021-03-22 21:23:57 -0400

    google assistant complete

--- src/main.py
+++ src/main.py
@@ -21,12 +21,11 @@ def talker():
 
 
 class RosAssistant(object):
-    def __init__(self, language_code, device_model_id, device_id, conversation_stream, display):
+    def __init__(self, language_code, device_model_id, device_id, conversation_stream):
         self.language_code = language_code
         self.device_model_id = device_model_id
         self.device_id = device_id
         self.conversation_stream = conversation_stream
-        self.display = display
 
         self.conversation_state = None
         # Force reset of first conversation.
@@ -52,6 +51,98 @@ class RosAssistant(object):
             return True
         return False
 
+    @retry(reraise= True, stop=stop_after_attempt(3), retry=retry_if_exception(is_grpc_error_unavailable))
+    def assist(self):
+        continue_conversation = False
+        device_actions_futures = []
+
+        self.conversation_stream.start_recording()
+        logging.info('Recording audio request')
+
+        def iter_log_assist_requests():
+            for c in self.gen_assist_request():
+                assistant_helpers.iter_log_assist_request_without_audio(c)
+                yield c
+            logging.debug('Reached end of AssistRequest iteration')
+
+    
+    def gen_assist_request(self):
+        """Yields: AssistRequest messages to send to the API."""
+
+        config = embedded_assistant_pb2.AssistConfig(
+            audio_in_config=embedded_assistant_pb2.AudioInConfig(
+                encoding='LINEAR16',
+                sample_rate_hertz=self.conversation_stream.sample_rate,
+            ),
+            audio_out_config=embedded_assistant_pb2.AudioOutConfig(
+                encoding='LINEAR16',
+                sample_rate_hertz=self.conversation_stream.sample_rate,
+                volume_percentage=self.conversation_stream.volume_percentage,
+            ),
+            dialog_state_in=embedded_assistant_pb2.DialogStateIn(
+                language_code=self.language_code,
+                conversation_state=self.conversation_state,
+                is_new_conversation=self.is_new_conversation,
+            ),
+            device_config=embedded_assistant_pb2.DeviceConfig(
+                device_id=self.device_id,
+                device_model_id=self.device_model_id,
+            )
+        )
+
+        self.is_new_conversation = False
+        # The first AssistRequest must contain the AssistConfig
+        # and no audio data.
+        yield embedded_assistant_pb2.AssistRequest(config=config)
+        for data in self.conversation_stream:
+            # Subsequent requests need audio data, but not config.
+            yield embedded_assistant_pb2.AssistRequest(audio_in=data)
+    
+        for resp in self.assistant.Assist(iter_log_assist_requests(),
+                                          self.deadline):
+            assistant_helpers.log_assist_response_without_audio(resp)
+            if resp.event_type == END_OF_UTTERANCE:
+                logging.info('End of audio request detected.')
+                logging.info('Stopping recording.')
+                self.conversation_stream.stop_recording()
+            if resp.speech_results:
+                logging.info('Transcript of user request: "%s".',
+                             ' '.join(r.transcript
+                                      for r in resp.speech_results))
+            if len(resp.audio_out.audio_data) > 0:
+                if not self.conversation_stream.playing:
+                    self.conversation_stream.stop_recording()
+                    self.conversation_stream.start_playback()
+                    logging.info('Playing assistant response.')
+                self.conversation_stream.write(resp.audio_out.audio_data)
+            if resp.dialog_state_out.conversation_state:
+                conversation_state = resp.dialog_state_out.conversation_state
+                logging.debug('Updating conversation state.')
+                self.conversation_state = conversation_state
+            if resp.dialog_state_out.volume_percentage != 0:
+                volume_percentage = resp.dialog_state_out.volume_percentage
+                logging.info('Setting volume to %s%%', volume_percentage)
+                self.conversation_stream.volume_percentage = volume_percentage
+            if resp.dialog_state_out.microphone_mode == DIALOG_FOLLOW_ON:
+                continue_conversation = True
+                logging.info('Expecting follow-on query from user.')
+            elif resp.dialog_state_out.microphone_mode == CLOSE_MICROPHONE:
+                continue_conversation = False
+            if resp.device_action.device_request_json:
+                device_request = json.loads(
+                    resp.device_action.device_request_json
+                )
+                fs = self.device_handler(device_request)
+                if fs:
+                    device_actions_futures.extend(fs)
+            
+        if len(device_actions_futures):
+            logging.info('Waiting for device executions to complete.')
+            concurrent.futures.wait(device_actions_futures)
+
+        logging.info('Finished playing assistant response.')
+        self.conversation_stream.stop_playback()
+        return continue_conversation
 
 
 if __name__ == '__main__':
@@ -103,3 +194,18 @@ if __name__ == '__main__':
     )
 
     device_handler = device_helpers.DeviceRequestHandler(device_id)
+
+    ros_assistant = RosAssistant(device_model_id, device_id, conversation_stream)
+
+    pub = rospy.Publisher("chatter", String, queue_size=5)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+
+        continue_conversation = assistant.assist()
+
+
+        rate.sleep()
